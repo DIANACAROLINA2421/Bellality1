727b04431076960b1bdfe31a17a7664f28420af5
DIANACAROLINA2421 <carolinapita70@gmail.com>
2026-05-08 10:53:00 +0200

    Add

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -9,9 +8,7 @@ try:
 except ImportError:
     pass
 
-
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bellality.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -22,7 +19,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
