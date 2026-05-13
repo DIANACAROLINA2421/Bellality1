35b853dfed809cb0f0000c13e74e53196dd2d0a0
DIANACAROLINA2421 <carolinapita70@gmail.com>
2026-05-13 12:24:01 +0200

    subida

--- Bellality/urls.py
+++ Bellality/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('api/', include('Users.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
