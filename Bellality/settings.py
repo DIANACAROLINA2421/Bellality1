1c518c42ba99ec11d9971fc67be7362dcfdf9448
DIANACAROLINA2421 <carolinapita70@gmail.com>
2026-05-15 22:21:16 +0200

    fds

--- Bellality/settings.py
+++ Bellality/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # ✅ cloudinary_storage DEBE ir ANTES de django.contrib.staticfiles
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
