227ed20d899ae76f790f0b8958e7251c2e97225c
DIANACAROLINA2421 <carolinapita70@gmail.com>
2026-05-17 12:10:07 +0200

    dfgt

--- Bellality/settings.py
+++ Bellality/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from decouple import config
 import os
 
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", "")
 DEBUG = config("DEBUG", default=True, cast=bool)
